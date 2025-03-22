1074906e8ec498c49d2d6b6659ca5e264f03dde1
MtDewWolf <mtdewwolf123233@gmail.com>
2025-03-22 14:45:29 -0600

    Fix: Add 'use client' directive to context files

--- src/contexts/SocketContext.tsx
+++ src/contexts/SocketContext.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React, { createContext, useContext, useEffect, useState, ReactNode } from 'react';
 import { io, Socket } from 'socket.io-client';
 import { Game } from '@/types';
